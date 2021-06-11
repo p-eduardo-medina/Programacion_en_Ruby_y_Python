12f3f0eb51101ecbc82a837de8e1206336e7a176
p-eduardo-medina <83928073+p-eduardo-medina@users.noreply.github.com>
2021-06-10 19:06:19 -0500

    Aritmética

--- Problemas aritmeticos.py
+++ Problemas aritmeticos.py
@@ -21,6 +21,7 @@ def Numprimos(n):
         n /= 2
     f = 3   
     while f * f <= n:        
+        print(f,n)
         if n % f == 0:
             a.append(f)
             n /= f
@@ -31,4 +32,15 @@ def Numprimos(n):
     return a
 # a.keys()   a.values()
 def Nnumerosprimos(num):
-    
+    i=0 
+    PN = []
+    while len(PN)<num:
+        i+=1
+        PN = (Numprimos(i)).keys()
+    return PN
+
+
+#a = Nnumerosprimos(8)
+a = Numprimos(24)
+print(a)
+        
